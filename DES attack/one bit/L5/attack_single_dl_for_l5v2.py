ebc11031450834820e7ab5a2178bcc21915a17dc
HuaHuaNiu <103615002+Monkey2022@users.noreply.github.com>
2023-12-10 19:44:23 +0800

    Add files via upload

---
+++ DES attack/one bit/L5/attack_single_dl_for_l5v2.py
@@ -0,0 +1,125 @@
+# -*- coding: utf-8 -*-
+
+
+
+import numpy as np
+from os import urandom
+
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"#
+
+import tensorflow as tf  
+config = tf.compat.v1.ConfigProto(allow_soft_placement=True)  
+config.gpu_options.allow_growth=True  
+session = tf.compat.v1.Session(config=config)  
+tf.compat.v1.keras.backend.set_session(session)#
+
+from tensorflow.keras.models import model_from_json
+
+import des_for_linear as ciph
+#only for 5-round linear expression of des
+
+
+
+#
+def get_Round():
+    return 5
+
+
+def obtain_subkey_use_L5(p,c,sample_pair,pc_pair):
+    
+
+    #
+    single_json_file = open('./neural_network/single_L_'+str(get_Round())+'_'+str(pc_pair)+'v2_model.json','r')
+    single_json_model = single_json_file.read()
+    single_net = model_from_json(single_json_model)
+    single_net.load_weights('./neural_network/single_L_'+str(get_Round())+'_'+str(pc_pair)+'v2_weight.h5')
+    
+    plain=p
+    cipher=c
+    
+    Ph=np.array(plain[:,:32])
+    Pl=np.array(plain[:,32:])
+    
+    Ch=np.array(cipher[:,:32])
+    Cl=np.array(cipher[:,32:])
+    
+
+    
+    sample=Ph[:,31-15]^Pl[:,31-7]^Pl[:,31-18]^Pl[:,31-24]^Pl[:,31-27]^Pl[:,31-28]^Pl[:,31-29]^Pl[:,31-30]^Pl[:,31-31]^Ch[:,31-15]^Cl[:,31-7]^Cl[:,31-18]^Cl[:,31-24]^Cl[:,31-27]^Cl[:,31-28]^Cl[:,31-29]^Cl[:,31-30]^Cl[:,31-31]
+    
+    sample=sample.reshape(-1,pc_pair)
+    
+    result=single_net.predict(sample,batch_size=2**18)
+    
+    result=result.flatten()
+    result=result/(1-result)
+    result = np.log2(result)
+    result=np.mean(result)
+    
+    if(result<0):
+        return 1
+    else:
+        return 0
+    
+ 
+
+
+def attack_5_round_use_L5(pc_pair,sample_pair):
+    
+    
+
+    keys = np.frombuffer(urandom(64), dtype=np.uint8).reshape(-1,64)
+    keys = keys & 1
+    subkey=ciph.expand_key(keys,get_Round())#攻击的轮数比等式的轮数多1轮
+    
+    #
+    plain = np.frombuffer(urandom(64*sample_pair*pc_pair), dtype=np.uint8).reshape(-1,64)
+    plain = plain & 1
+
+    Ph=np.array(plain[:,:32])
+    Pl=np.array(plain[:,32:])
+    #
+    for sk in subkey:
+        Ph,Pl = ciph.enc_one_round((Ph,Pl), sk)
+    cipher=np.concatenate((Pl,Ph),axis=1)#
+    
+    real_key=subkey[0][:,47-42]^subkey[0][:,47-43]^subkey[0][:,47-45]^subkey[0][:,47-46]^subkey[1][:,47-22]^subkey[3][:,47-22]^subkey[4][:,47-42]^subkey[4][:,47-43]^subkey[4][:,47-45]^subkey[4][:,47-46]
+    real_key=real_key[0]
+    
+    
+    guess_key=obtain_subkey_use_L5(plain,cipher,sample_pair,pc_pair)
+
+    return real_key,guess_key
+    
+
+num=10**4
+
+pr=0.5+1.22*pow(2,-6)
+num_pair=pow(abs(pr-0.5),-2)
+
+
+Pc_pair=[16,20,22,24,26,28,30,32,34,52,60,70,80,90,100,110,120,130,140]
+Sample_pair=[1]
+
+for pc_pair in Pc_pair:
+    for sample_pair in Sample_pair:
+        
+        flag=0
+        
+        Real_key=[]
+        Guess_key=[]
+
+        for i in range(num):   
+            real_key,guess_key=attack_5_round_use_L5(pc_pair,int(num_pair/pc_pair))
+            Real_key.append(real_key)
+            Guess_key.append(guess_key)
+            if(real_key==guess_key):
+                flag=flag+1
+            print(i,flag/(i+1))
+        print(flag/num)
+        np.save('real_key_for_dl_dim='+str(pc_pair)+'_'+str(int(num_pair/pc_pair))+'.npy',np.array(Real_key,dtype=np.uint8))
+        np.save('guess_key_for_dl_dim='+str(pc_pair)+'_'+str(int(num_pair/pc_pair))+'.npy',np.array(Guess_key,dtype=np.uint8))
+    
+    
+    
---
+++ DES attack/one bit/L5/attack_single_mastui_for_l5v2.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+from os import urandom
+
+
+
+import des_for_linear as ciph
+#only for 5-round linear expression of des
+
+
+def get_Round():
+    return 5
+
+
+def obtain_subkey_use_L5(p,c):
+    
+
+    pr=0.5+1.22*pow(2,-6)
+    
+    plain=p
+    cipher=c
+    
+    Ph=np.array(plain[:,:32])
+    Pl=np.array(plain[:,32:])
+    
+    Ch=np.array(cipher[:,:32])
+    Cl=np.array(cipher[:,32:])
+    
+
+    #Guess_expression_key=0
+    
+    sample=Ph[:,31-15]^Pl[:,31-7]^Pl[:,31-18]^Pl[:,31-24]^Pl[:,31-27]^Pl[:,31-28]^Pl[:,31-29]^Pl[:,31-30]^Pl[:,31-31]^Ch[:,31-15]^Cl[:,31-7]^Cl[:,31-18]^Cl[:,31-24]^Cl[:,31-27]^Cl[:,31-28]^Cl[:,31-29]^Cl[:,31-30]^Cl[:,31-31]
+    
+    sample=sample.flatten()
+    sample=np.array(sample,dtype=np.uint32)
+    
+    T=len(sample)-sum(sample)
+    if(T>0.5*len(sample)):
+        if(pr>0.5):
+            return 0
+        else:
+            return 1
+    else:
+        if(pr>0.5):
+            return 1
+        else:
+            return 0
+    
+ 
+
+
+def attack_5_round_use_L5(num_pair):
+
+
+    keys = np.frombuffer(urandom(64), dtype=np.uint8).reshape(-1,64)
+    keys = keys & 1
+    subkey=ciph.expand_key(keys,get_Round())#
+
+    plain = np.frombuffer(urandom(64*num_pair), dtype=np.uint8).reshape(-1,64)
+    plain = plain & 1
+
+    Ph=np.array(plain[:,:32])
+    Pl=np.array(plain[:,32:])
+
+    for sk in subkey:
+        Ph,Pl = ciph.enc_one_round((Ph,Pl), sk)
+    cipher=np.concatenate((Pl,Ph),axis=1)#
+    
+    real_key=subkey[0][:,47-42]^subkey[0][:,47-43]^subkey[0][:,47-45]^subkey[0][:,47-46]^subkey[1][:,47-22]^subkey[3][:,47-22]^subkey[4][:,47-42]^subkey[4][:,47-43]^subkey[4][:,47-45]^subkey[4][:,47-46]
+    real_key=real_key[0]
+    
+    
+    guess_key=obtain_subkey_use_L5(plain,cipher)
+
+    
+    return real_key,guess_key
+    
+
+num=10**5
+flag=0
+
+
+num_pair=4317#
+
+Real_key=[]
+Guess_key=[]
+
+NUM_pair=[int(num_pair)]
+
+for num_pair in NUM_pair:
+
+    for i in range(num): 
+    
+        real_key,guess_key=attack_5_round_use_L5(num_pair)
+        Real_key.append(real_key)
+        Guess_key.append(guess_key)
+        np.save('real_key_for_mastui'+str(num_pair)+'.npy',np.array(Real_key,dtype=np.uint8))
+        np.save('guess_key_for_mastui'+str(num_pair)+'.npy',np.array(Guess_key,dtype=np.uint8))
+
+    
+        if(real_key==guess_key):
+            flag=flag+1
+        print(i,flag/(i+1))
+
+print(flag/num)
+    
+    
+    
